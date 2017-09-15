337b27186df0bb584b573fa3cacc287c4728abb9
= <pangkeyd@gmail.com>
2017-09-15 11:33:12 +0700

    third-commit

--- routes/supplierItem.js
+++ routes/supplierItem.js
@@ -4,6 +4,7 @@ const models = require('../models')
 
 routers.get('/', function(req, res){
   models.SupplierItem.findAll({
+    attributes: ['id'],
     include: [{
       model: models.Suppliers,
       include: [
@@ -12,7 +13,30 @@ routers.get('/', function(req, res){
     }]
   })
   .then(function(alldata){
+    // res.send(alldata)
     res.render('supplierItem', {data: alldata, title: 'All Data'})
+  })
+})
+
+routers.get('/tambah', function(req, res){
+  models.Suppliers.findAll()
+  .then(function(supp){
+    models.Item.findAll()
+    .then(function(item){
+      res.render('tambahSupplierItem', {data: supp, dataItem: item, title: 'Tambah Data'})
+    })
+  })
+})
+
+routers.post('/tambah', function(req, res){
+  models.SupplierItem.build({
+    SupplierId: req.body.supp,
+    ItemId: req.body.item,
+    price: req.body.price
+  })
+  .save().then(function(all){
+    // res.send(all)
+    res.redirect('/supplieritem')
   })
 })
 
